91ab9e277f91c4c5c17a90ce33d94c86864de0fe
Raul Sanchez <raul@um.es>
2012-12-04 13:20:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeedupRailsTests::Application.config.secret_token = 'a3b6c14a9757d1176e382e005f8d96908549375b6a74154f6746ba569daf307eb9640b7387b15fde1f3523c7ffe6bf022862925f116d102d1ca02b360a5bfcaa'
